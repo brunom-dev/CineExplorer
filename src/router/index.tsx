797f4809feb8ea3947c219c9c80e87c84cf5bfdd
Bruno Macedo <devbrunomacedo@gmail.com>
2025-06-27 16:45:29 -0300

    Configuração inicial das Rotas e Layout

---
+++ src/router/index.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom"
+import { Layout } from "../components/Layout"
+
+import { Home } from "../pages/Home"
+
+export const router = createBrowserRouter([
+    {
+        element: <Layout />,
+
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+
+        ],
+    }
+])
