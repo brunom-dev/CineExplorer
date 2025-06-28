4ae4d025a34a3129d2800f6706dd48ed4501d3ef
Bruno Macedo <devbrunomacedo@gmail.com>
2025-06-28 12:31:25 -0300

    Criação do type VideoProps

---
+++ src/types/VideoProps.ts
@@ -0,0 +1,9 @@
+export type VideoProps = {
+    id: string;
+    iso_639_1: string;
+    key: string; // chave youtube
+    name: string;
+    official: boolean;
+    site: string;
+    type: string;
+};
