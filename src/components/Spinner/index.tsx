86296e0ccd2cb6fb799899ed3757b634e108827e
Bruno Macedo <devbrunomacedo@gmail.com>
2025-06-28 12:30:35 -0300

    Componente de Spinner para carregamento da pagina

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,11 @@
+
+
+
+export const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="w-16 h-16 border-4 border-solid border-slate-600 border-t-sky-600 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
