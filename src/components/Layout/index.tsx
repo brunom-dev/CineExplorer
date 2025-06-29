1d815a2353e70a76c4d0113045c6532345882385
Bruno Macedo <devbrunomacedo@gmail.com>
2025-06-29 14:44:16 -0300

    Solução de bug de scroll ao mudar de pagina

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from "../Header"
 import { Outlet } from "react-router-dom";
 import { Footer } from '../Footer/index';
+import { ScrollToTop } from "../ScrollToTop"
 
 
 
 export function Layout() {
     return (
-        <>
+        <>  
+            <ScrollToTop />
             <Header />
             <main className="mv-">
                 <Outlet />
