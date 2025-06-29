cc17306deee49d2998a277178db54bf88aa5fcf2
Bruno Macedo <devbrunomacedo@gmail.com>
2025-06-29 14:01:11 -0300

    adição de uma nova rota para a pagina de detalhes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom"
 import { Layout } from "../components/Layout"
 
 import { Home } from "../pages/Home"
+import { MediaDetails } from '../pages/MediaDetails/index';
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
                 path: "/",
                 element: <Home />
             },
+            {
+                path: "/movie/:id",
+                element: <MediaDetails />
+            }
 
         ],
     }
