c6d62a810647bd5062982f50efa50a00ff4750b8
Bruno Macedo <devbrunomacedo@gmail.com>
2025-07-06 12:09:20 -0300

    Componente SkeletonCard

---
+++ src/components/SkeletonCard/index.tsx
@@ -0,0 +1,9 @@
+export const SkeletonCard = () => {
+    return (
+        <div className="w-full bg-slate-900 rounded-xl overflow-hidden">
+            <div className="aspect-[3/4] w-full bg-slate-700 animate-pulse">
+            
+            </div>
+        </div>
+    );
+};
