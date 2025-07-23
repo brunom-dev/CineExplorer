663321db80c3a57ddf30b4fcc555d7be555e8c63
Bruno Macedo <devbrunomacedo@gmail.com>
2025-07-23 20:58:41 -0300

    feat: contexto de auth

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,63 @@
+import {
+    createContext,
+    useContext,
+    useState,
+    useEffect,
+    type ReactNode,
+} from "react";
+
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
+import { db, auth } from "../services/firebase/firebaseConfig";
+
+import type { UserProps as AppUser } from "../types/User/UserProps";
+import { doc, getDoc } from "firebase/firestore";
+
+interface AuthContextType {
+    currentUser: AppUser | null;
+    firebaseUser: User | null;
+    isLoading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+    const [currentUser, setCurrentUser] = useState<AppUser | null>(null);
+    const [firebaseUser, setFirebaseUser] = useState<User | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                setFirebaseUser(user);
+                const userDocRef = doc(db, "users", user.uid);
+                const userDocSnap = await getDoc(userDocRef);
+                if (userDocSnap.exists()) {
+                    setCurrentUser({
+                        uid: user.uid,
+                        ...userDocSnap.data(),
+                    } as AppUser);
+                }
+            } else {
+                setFirebaseUser(null);
+                setCurrentUser(null);
+            }
+            setIsLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const value = { currentUser, firebaseUser, isLoading };
+
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+};
